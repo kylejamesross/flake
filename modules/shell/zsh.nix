6ff791bea07fe85d8fb33d2c3044bf13397b31b8
Kyle Ross <kylejamesross@gmail.com>
2023-06-25 22:02:47 -0600

    Adjust zsh and add key

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -27,25 +27,28 @@
       };
 
       shellInit = ''
-if [ -f "$HOME/.dotfiles/zsh/feed-access-token" ]; then
-  export FEED_ACCESSTOKEN=$(cat $HOME/.dotfiles/zsh/feed-access-token)
-  export ENV DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
-  export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS="{\"endpointCredentials\": [{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/NuAgile/_packaging/NudeSolutions/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.dotfiles/zsh/feed-access-token)\"},{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/ProjectVicious/_packaging/NudeGet/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.dotfiles/zsh/feed-access-token)\"}]}"
-fi
+        if [ -f "$HOME/.feed-access-token" ]; then
+          export FEED_ACCESSTOKEN=$(cat $HOME/.feed-access-token)
+          export ENV DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
+          export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS="{\"endpointCredentials\": [{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/NuAgile/_packaging/NudeSolutions/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.feed-access-token)\"},{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/ProjectVicious/_packaging/NudeGet/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.dotfiles/zsh/feed-access-token)\"}]}"
+        fi
 
-# PATH
-PATH="$PATH:$HOME/.local/bin"
-PATH="$PATH:$HOME/.dotfiles/bin"
-PATH="$PATH:/usr/share/dotnet"
-PATH="$PATH:$HOME/.dotnet/tools"
+        if [ -f "$HOME/.openai-api-key" ]; then
+          export OPENAI_API_KEY=$(cat $HOME/.openai-api-key)
+        fi
 
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-setopt APPEND_HISTORY
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY
-autoload -Uz compinit && compinit
+        PATH="$PATH:$HOME/.local/bin"
+        PATH="$PATH:$HOME/.dotfiles/bin"
+        PATH="$PATH:/usr/share/dotnet"
+        PATH="$PATH:$HOME/.dotnet/tools"
+
+        HISTFILE=~/.zsh_history
+        HISTSIZE=10000
+        SAVEHIST=10000
+        setopt APPEND_HISTORY
+        setopt INC_APPEND_HISTORY
+        setopt SHARE_HISTORY
+        autoload -Uz compinit && compinit
       '';
     };
   };
