9b28624004298e760503f2692df03c0d8026ded4
Kyle Ross <kylejamesross@gmail.com>
2023-06-25 22:37:11 -0600

    Update install secrets and add config lazygit back in

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -30,7 +30,7 @@
         if [ -f "$HOME/.feed-access-token" ]; then
           export FEED_ACCESSTOKEN=$(cat $HOME/.feed-access-token)
           export ENV DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
-          export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS="{\"endpointCredentials\": [{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/NuAgile/_packaging/NudeSolutions/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.feed-access-token)\"},{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/ProjectVicious/_packaging/NudeGet/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.dotfiles/zsh/feed-access-token)\"}]}"
+          export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS="{\"endpointCredentials\": [{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/NuAgile/_packaging/NudeSolutions/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.feed-access-token)\"},{\"endpoint\":\"https://pkgs.dev.azure.com/nueradev/ProjectVicious/_packaging/NudeGet/nuget/v3/index.json\", \"username\":\"docker\", \"password\":\"$(cat $HOME/.feed-access-token)\"}]}"
         fi
 
         if [ -f "$HOME/.openai-api-key" ]; then
