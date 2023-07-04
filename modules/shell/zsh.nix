75ce2d9a02c9d9c21880fd19c01f2ab39ead5e5d
Kyle Ross <kylejamesross@gmail.com>
2023-07-04 13:45:00 -0600

    Remove comment

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -8,7 +8,7 @@
   programs = {
     zsh = {
       enable = true;
-      autosuggestions.enable = true;            # Auto suggest options and highlights syntax, searches in history for options
+      autosuggestions.enable = true;
       syntaxHighlighting.enable = true;
       enableCompletion = true;
       histSize = 100000;
