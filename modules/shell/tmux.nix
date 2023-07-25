220459f94c5327974cab83ea83cbab01207106d8
Kyle Ross <kylejamesross@gmail.com>
2023-07-24 22:58:53 -0600

    Fix btop and config

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -23,7 +23,7 @@
         bind-key -r h run-shell "tmux neww ~/.dotfiles/bin/cht"
         bind-key -r v run-shell "tmux neww -S -n lazygit lazygit"
         bind-key -r , run-shell "tmux neww -S -n joplin joplin"
-        bind-key -r . run-shell "tmux neww -S -n bpytop bpytop"
+        bind-key -r . run-shell "tmux neww -S -n bpytop btop"
         bind-key -r R run-shell "tmux source ~/.tmux.conf"
         bind-key -r / run-shell "tmux neww -S -n playground"
         bind-key -r t run-shell "tmux neww -S -n terminal"
