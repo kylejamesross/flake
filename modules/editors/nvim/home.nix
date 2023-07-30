f3db1979a3e822b9e9df5e30acd85dbcd67732f5
Kyle Ross <kylejamesross@gmail.com>
2023-07-30 16:19:13 -0600

    Do not load after plugins in nvim nix

--- modules/editors/nvim/home.nix
+++ modules/editors/nvim/home.nix
@@ -85,26 +85,6 @@ in
       extraConfig = ''
       :luafile ~/.config/nvim/lua/user/keymaps.lua
       :luafile ~/.config/nvim/lua/user/options.lua
-      :luafile ~/.config/nvim/after/plugin/alpha.lua
-      :luafile ~/.config/nvim/after/plugin/autopairs.lua
-      :luafile ~/.config/nvim/after/plugin/bufferline.lua
-      :luafile ~/.config/nvim/after/plugin/colorizer.lua
-      :luafile ~/.config/nvim/after/plugin/comment.lua
-      :luafile ~/.config/nvim/after/plugin/dracula.lua
-      :luafile ~/.config/nvim/after/plugin/gitsigns.lua
-      :luafile ~/.config/nvim/after/plugin/illuminate.lua
-      :luafile ~/.config/nvim/after/plugin/impatient.lua
-      :luafile ~/.config/nvim/after/plugin/indent-blankline.lua
-      :luafile ~/.config/nvim/after/plugin/lsp.lua
-      :luafile ~/.config/nvim/after/plugin/lualine.lua
-      :luafile ~/.config/nvim/after/plugin/luasnip.lua
-      :luafile ~/.config/nvim/after/plugin/nvim-tree.lua
-      :luafile ~/.config/nvim/after/plugin/scrollbar.lua
-      :luafile ~/.config/nvim/after/plugin/surround.lua
-      :luafile ~/.config/nvim/after/plugin/telescope.lua
-      :luafile ~/.config/nvim/after/plugin/treesitter.lua
-      :luafile ~/.config/nvim/after/plugin/typescript.lua
-      :luafile ~/.config/nvim/after/plugin/which-key.lua
         '';
     };
   };
