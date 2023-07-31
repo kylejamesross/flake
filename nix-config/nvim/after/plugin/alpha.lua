6497754fc0733b170cc20cd77379c896c721647b
Kyle Ross <kylejamesross@gmail.com>
2023-07-30 21:59:08 -0600

    Adjust alpha so things make sense

--- nix-config/nvim/after/plugin/alpha.lua
+++ nix-config/nvim/after/plugin/alpha.lua
@@ -5,11 +5,10 @@ end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.buttons.val = {
-	dashboard.button("f", "󰍉" .. " Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "" .. " New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "󰍉" .. " Find file", ":Telescope find_files <CR>"),
+	dashboard.button("a", "" .. " New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("r", "" .. " Recent files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "󰊄" .. " Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "" .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
 	dashboard.button("q", "" .. " Quit", ":qa<CR>"),
 }
 local function footer()
