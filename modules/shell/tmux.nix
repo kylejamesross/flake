a9fd958da6624d751f8f35e1810d41470bf60e1b
Kyle Ross <kylejamesross@gmail.com>
2023-08-08 21:16:39 -0600

    Add change dotfiles to flake

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -19,8 +19,8 @@
         set-option -g focus-events on
         set-option -sa terminal-overrides ",alacritty:RGB"
 
-        bind-key -r f run-shell "tmux neww ~/.dotfiles/bin/tmux-sessionizer"
-        bind-key -r h run-shell "tmux neww ~/.dotfiles/bin/cht"
+        bind-key -r f run-shell "tmux neww ~/flake/bin/tmux-sessionizer"
+        bind-key -r h run-shell "tmux neww ~/flake/bin/cht"
         bind-key -r v run-shell "tmux neww -S -n lazygit lazygit"
         # bind-key -r , run-shell "tmux neww -S -n joplin joplin"
         bind-key -r . run-shell "tmux neww -S -n bpytop btop"
