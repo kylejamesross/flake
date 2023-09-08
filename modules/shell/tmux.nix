f47472df4a39e7fc37cc6439d0f08fad8ccaac90
Kyle Ross <kylejamesross@gmail.com>
2023-09-08 14:23:05 -0600

    Add yanking and visual selection of text in tmux

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -19,6 +19,9 @@
         set-option -g focus-events on
         set-option -sa terminal-overrides ",alacritty:RGB"
 
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
         bind-key -r f run-shell "tmux neww ~/flake/bin/tmux-sessionizer"
         bind-key -r h run-shell "tmux neww ~/flake/bin/cht"
         bind-key -r v run-shell "tmux neww -S -n lazygit lazygit"
