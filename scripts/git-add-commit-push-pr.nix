f8db1d36ce3f5d512fd40af2759e595ce4e99c8b
Kyle Ross <kylejamesross@gmail.com>
2024-01-27 22:14:31 -0700

    AB#3: Add hub and gh to the list of installed packages in home.nix - hub and gh are added to the list of installed packages in home.nix file.
    Update the sourcing of .p10k.zsh in .zshrc
    - The sourcing of .p10k.zsh in .zshrc file is updated to use the correct syntax.
    
    Update the git-add-commit-push-pr.nix script
    - The script now extracts the title and body from the commit message.
    - The command to create a pull request is updated to use the `gh` command instead of `hub`.
    - The pull request URL is copied to the clipboard.
    - The pull request URL is opened in the default web browser.
    
    Note: The commit message format is not provided, so the actual content of the commit message is not included in the explanation.

--- scripts/git-add-commit-push-pr.nix
+++ scripts/git-add-commit-push-pr.nix
@@ -5,16 +5,17 @@ pkgs.writeShellScriptBin "gcp" ''
 
 commit_message=$(work-commit-message)
 
+title=$(echo "$commit_message" | head -n 1)
+body=$(echo "$commit_message" | tail -n +2)
+
 git add .
 git commit -m "$commit_message"
 
 git push origin HEAD
 
-pr_url=$(${pkgs.hub}/bin/hub pull-request -m "$commit_message" -b origin/main -h HEAD)
+pr_url=$(${pkgs.gh}/bin/gh pr create --web --title "$title" --body "$body" -b origin/main -h HEAD | tail -n 1)
+echo "$title: $pr_url" | wl-copy
+git web--browse "$pr_url"
 
-echo "$pr_url" | wl-copy
-
-echo "Pull request created: $pr_url"
-echo "PR title and link copied to clipboard."
 ''
 
