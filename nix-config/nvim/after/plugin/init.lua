274b3615e167b7faf1d5c8973140b79ffe847064
Kyle Ross <kylejamesross@gmail.com>
2024-03-06 13:07:03 -0700

    nvim autopairs adjust

--- nix-config/nvim/after/plugin/init.lua
+++ nix-config/nvim/after/plugin/init.lua
@@ -6,4 +6,5 @@ require("noice").setup({})
 require("scrollbar").setup({})
 require("scrollbar.handlers.gitsigns").setup()
 require("nvim-surround").setup({})
+require("nvim-autopairs").setup({ check_ts = true })
 
