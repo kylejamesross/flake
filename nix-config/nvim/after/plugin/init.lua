d2f904fce838e14271c54e8142882a57d3d2945d
Kyle Ross <kylejamesross@gmail.com>
2024-03-22 19:24:59 -0600

    Fix autocomplete on single react nodes

--- nix-config/nvim/after/plugin/init.lua
+++ nix-config/nvim/after/plugin/init.lua
@@ -4,6 +4,6 @@ require("Comment").setup()
 require("fidget").setup({})
 require("scrollbar").setup()
 require("scrollbar.handlers.gitsigns").setup()
-require("nvim-autopairs").setup({ check_ts = true })
+require("nvim-autopairs").setup({ check_ts = true, ts_config = { java = false }})
 require('mini.ai').setup { n_lines = 500 }
 require('mini.surround').setup()
