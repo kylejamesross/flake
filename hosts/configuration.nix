059ad89aaa38652a69e61330b21368189e840f96
Kyle Ross <kylejamesross@gmail.com>
2024-03-29 23:22:09 -0600

    Change permissions

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -128,6 +128,7 @@
         mkdir -p /home/${user}/.ssh/
         echo "$(cat ${config.sops.secrets.id_ed25519.path})" > /home/${user}/.ssh/id_ed25519
         chmod 600 /home/${user}/.ssh/id_ed25519
+        chmod 700 /home/${user}/.ssh/
         echo "$(cat ${config.sops.secrets.feed_access_token.path})" > /home/${user}/.feed-access-token
         echo "$(cat ${config.sops.secrets.openai_api_key.path})" > /home/${user}/.openai-api-key
       '';
