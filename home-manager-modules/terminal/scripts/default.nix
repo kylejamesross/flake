48a21e735520b2268ee76fe0bf868c5753017249
Kyle Ross <kylejamesross@gmail.com>
2024-04-07 21:48:08 -0600

    Move script into folders

--- home-manager-modules/terminal/scripts/default.nix
+++ home-manager-modules/terminal/scripts/default.nix
@@ -3,15 +3,15 @@
 {
     home = {
         packages = [
-            (import ./work-commit-message.nix { inherit pkgs; })
-            (import ./personal-commit-message.nix { inherit pkgs; })
-            (import ./tmux-sessionizer.nix { inherit pkgs; })
-            (import ./swww-randomizer.nix { inherit pkgs; })
-            (import ./git-pull-all.nix { inherit pkgs; })
-            (import ./git-add-commit-push-pr.nix { inherit pkgs; })
-            (import ./git-commit-push-pr.nix { inherit pkgs; })
-            (import ./git-push-pr.nix { inherit pkgs; })
-            (import ./unraid-mount-unmount.nix { inherit pkgs user; })
+            (import ./work-commit-message { inherit pkgs; })
+            (import ./personal-commit-message { inherit pkgs; })
+            (import ./tmux-sessionizer { inherit pkgs; })
+            (import ./swww-randomizer { inherit pkgs; })
+            (import ./git-pull-all { inherit pkgs; })
+            (import ./git-add-commit-push-pr { inherit pkgs; })
+            (import ./git-commit-push-pr { inherit pkgs; })
+            (import ./git-push-pr { inherit pkgs; })
+            (import ./unraid-mount-unmount { inherit pkgs user; })
         ];
     };
 }
--- home-manager-modules/terminal/scripts/unraid-mount-unmount/default.nix
+++ home-manager-modules/terminal/scripts/unraid-mount-unmount/default.nix
@@ -1,4 +1,3 @@
-
 { pkgs, user }:
 
 pkgs.writeShellScriptBin "unraid" ''
