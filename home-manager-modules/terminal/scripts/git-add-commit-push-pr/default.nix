ac0bdeb22573332fe2d1af2963e3005c747f4e9e
Kyle Ross <kylejamesross@gmail.com>
2024-05-09 23:01:37 -0600

    Enhance github push script

--- home-manager-modules/terminal/scripts/git-add-commit-push-pr/default.nix
+++ home-manager-modules/terminal/scripts/git-add-commit-push-pr/default.nix
@@ -4,7 +4,7 @@ pkgs.writeShellScriptBin "gacp" ''
 
 ${pkgs.git}/bin/git add -A
 
-${pkgs.git}/bin/git commit -e -m "$(work-commit-message)"
+${pkgs.git}/bin/git diff-index --quiet HEAD || ${pkgs.git}/bin/git commit -e -m "$(work-commit-message)"
 
 if [ $? -eq 1 ]; then
     exit 1
@@ -17,9 +17,14 @@ body=$(echo "$commit_message" | tail -n +2)
 
 ${pkgs.git}/bin/git push origin HEAD
 
-base_branch=$(${pkgs.git}/bin/git branch -r | ${pkgs.fzf}/bin/fzf)
+base_branch=$(${pkgs.git}/bin/git rev-parse --abbrev-ref @{upstream} | ${pkgs.gnused}/bin/sed 's/origin\///')
+head_branch=$(${pkgs.git}/bin/git rev-parse --abbrev-ref HEAD)
 
-pr_url=$(${pkgs.gh}/bin/gh pr create --title $title --body $body --base $base_branch | tail -n 1)
+pr_url=$(${pkgs.gh}/bin/gh pr create --title "$title" --body "$body" --head "$head_branch" --base "$base_branch" | tail -n 1)
+
+if [ $? -eq 1 ]; then
+    exit 1
+fi
 
 echo "$title - $pr_url" | wl-copy
 
