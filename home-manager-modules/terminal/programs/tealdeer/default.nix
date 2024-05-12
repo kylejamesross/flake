7331672169598549afd2f61c4a86ce5e6ec449b4
Kyle Ross <kylejamesross@gmail.com>
2024-05-11 19:12:03 -0600

    Insert colors into tealdeer

--- home-manager-modules/terminal/programs/tealdeer/default.nix
+++ home-manager-modules/terminal/programs/tealdeer/default.nix
@@ -1,4 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, config, inputs, lib, ... }:
+
+with config.colorScheme.palette;
+with inputs.nix-colors.lib-core.conversions;
+with lib;
+
+let 
+    base08RGB = hexToRGB base08;
+    base0BRGB = hexToRGB base0B;
+    base0DRGB = hexToRGB base0D;
+in
 {
     home = {
         packages = with pkgs; [
@@ -6,5 +16,25 @@
         ];
     };
 
-  xdg.configFile."tealdeer/config.toml".source = ./config.toml;
+  xdg.configFile."tealdeer/config.toml".text = ''
+    [display]
+    compact = false
+    use_pager = false
+
+    [style.command_name]
+    foreground = { rgb = { r = ${toString ( elemAt base08RGB 0 )}, g = ${toString ( elemAt base08RGB 1 )}, b = ${toString ( elemAt base08RGB 2 ) } } }
+
+    [style.example_text]
+    foreground = { rgb = { r = ${toString ( elemAt base0BRGB 0 )}, g = ${toString ( elemAt base0BRGB 1 )}, b = ${toString ( elemAt base0BRGB 2 ) } } }
+
+    [style.example_code]
+    foreground = { rgb = { r = ${toString ( elemAt base0DRGB 0 )}, g = ${toString ( elemAt base0DRGB 1 )}, b = ${toString ( elemAt base0DRGB 2 ) } } }
+
+    [style.example_variable]
+    foreground = { rgb = { r = ${toString ( elemAt base0DRGB 0 )}, g = ${toString ( elemAt base0DRGB 1 )}, b = ${toString ( elemAt base0DRGB 2 ) } } }
+    underline = true
+
+    [updates]
+    auto_update = true
+  '';
 }
