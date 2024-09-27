ac08903972d6f8cb83a13225a4d7fdae8fd0cb89
Kyle Ross <github@kylejamesross.com>
2024-09-26 23:58:14 -0600

    Allow disabling of auto formatting

--- home-manager-modules/terminal/programs/nvim/plugins/conform/default.nix
+++ home-manager-modules/terminal/programs/nvim/plugins/conform/default.nix
@@ -4,12 +4,46 @@
   ...
 }: {
   programs.nixvim = {
+    extraConfigLua = ''
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+         if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
+      vim.api.nvim_create_user_command("FormatToggle", function(args)
+        if args.bang then
+          -- Toggle formatting for current buffer
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
+        else
+          -- Toggle formatting globally
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+        end
+      end, {
+        desc = "Toggle autoformat-on-save",
+        bang = true,
+      })
+    '';
     plugins.conform-nvim = {
       enable = true;
       notifyOnError = true;
       formatOnSave = ''
         function(bufnr)
             local disable_filetypes = { c = true, cpp = true }
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
             return {
                 timeout_ms = 500,
                 lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
