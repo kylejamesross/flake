0df51aea8f727ead65608638ada0699b6b30f2b3
Kyle Ross <github@kylejamesross.com>
2024-10-06 00:41:09 -0600

    Add xbox wireless adapter support

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,12 +1,11 @@
-{ ... }:
+{...}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./config
+  ];
 
-{
-    imports = [
-        ./hardware-configuration.nix
-        ./config
-    ];
-
-    boot.initrd.kernelModules = [ "amdgpu" ];
-    services.xserver.videoDrivers = [ "amdgpu" ];
-    networking.hostName = "desktop";
+  boot.initrd.kernelModules = ["amdgpu"];
+  services.xserver.videoDrivers = ["amdgpu"];
+  networking.hostName = "desktop";
+  hardware.xone.enable = true;
 }
