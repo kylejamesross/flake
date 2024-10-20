59f4b40606e3c6c4a63b9e5cf18aec6074865861
Kyle Ross <github@kylejamesross.com>
2024-10-19 23:08:48 -0600

    Change state version again

--- home-manager-modules/home/default.nix
+++ home-manager-modules/home/default.nix
@@ -1,11 +1,9 @@
-{ user, ... }:
-
-{
-    home = {
-        username = "${user}";
-        homeDirectory = "/home/${user}";
-        stateVersion = "22.05";
-    };
-    programs.home-manager.enable = true;
-    stylix.targets.waybar.enable = false;
+{user, ...}: {
+  home = {
+    username = "${user}";
+    homeDirectory = "/home/${user}";
+    stateVersion = "24.05";
+  };
+  programs.home-manager.enable = true;
+  stylix.targets.waybar.enable = false;
 }
