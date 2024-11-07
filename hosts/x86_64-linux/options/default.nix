a969b0b0902a4dcd6b93d3c3d3fca7e90f60dc69
Kyle Ross <github@kylejamesross.com>
2024-11-06 19:36:13 -0700

    Fix quickfix lists and remove note options

--- hosts/x86_64-linux/options/default.nix
+++ hosts/x86_64-linux/options/default.nix
@@ -5,15 +5,4 @@
   ];
 
   options.ags.enable = lib.mkEnableOption "Whether to enable ags";
-
-  options.defaultNote = lib.mkOption {
-    type = lib.types.string;
-    default = "TODO.md";
-    description = "Default note file to open";
-  };
-  options.alternativeNote = lib.mkOption {
-    type = lib.types.string;
-    default = "WORK-TODO.md";
-    description = "Alternative note file to open";
-  };
 }
