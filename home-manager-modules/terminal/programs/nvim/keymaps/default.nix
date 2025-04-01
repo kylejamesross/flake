4e2e3c58b8db8efcfd51250f06fd3d96b4e98c46
Kyle Ross <github@kylejamesross.com>
2025-04-01 15:45:48 -0600

    Update keymaps, update snippets, update tmux sessionizer

--- home-manager-modules/terminal/programs/nvim/keymaps/default.nix
+++ home-manager-modules/terminal/programs/nvim/keymaps/default.nix
@@ -277,38 +277,38 @@
         key = "<Esc>";
         action = "<cmd>nohlsearch<CR>";
       }
-      {
-        mode = "n";
-        key = "<C-h>";
-        action = "<C-w><C-h>";
-        options = {
-          desc = "Move focus to the left window";
-        };
-      }
-      {
-        mode = "n";
-        key = "<C-l>";
-        action = "<C-w><C-l>";
-        options = {
-          desc = "Move focus to the right window";
-        };
-      }
-      {
-        mode = "n";
-        key = "<C-j>";
-        action = "<C-w><C-j>";
-        options = {
-          desc = "Move focus to the lower window";
-        };
-      }
-      {
-        mode = "n";
-        key = "<C-k>";
-        action = "<C-w><C-k>";
-        options = {
-          desc = "Move focus to the upper window";
-        };
-      }
+      # {
+      #   mode = "n";
+      #   key = "<C-h>";
+      #   action = "<C-w><C-h>";
+      #   options = {
+      #     desc = "Move focus to the left window";
+      #   };
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<C-l>";
+      #   action = "<C-w><C-l>";
+      #   options = {
+      #     desc = "Move focus to the right window";
+      #   };
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<C-j>";
+      #   action = "<C-w><C-j>";
+      #   options = {
+      #     desc = "Move focus to the lower window";
+      #   };
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<C-k>";
+      #   action = "<C-w><C-k>";
+      #   options = {
+      #     desc = "Move focus to the upper window";
+      #   };
+      # }
     ];
   };
 }
--- home-manager-modules/terminal/scripts/tmux-sessionizer/default.nix
+++ home-manager-modules/terminal/scripts/tmux-sessionizer/default.nix
@@ -1,16 +1,21 @@
 {pkgs}:
 pkgs.writeShellScriptBin "tmux-sessionizer" ''
+  session=$(${pkgs.fd}/bin/fd . "/home/$USER" --min-depth 1 --max-depth 3 --type d --exec sh -c 'test -e "$1/.git" && echo "$1"' sh {} \; | ${pkgs.fzf}/bin/fzf)
 
-  session=$(${pkgs.fd}/bin/fd . "/home/$USER" --min-depth 1 --max-depth 3 --type d --exec sh -c 'test -e "$1/.git" && echo "$1"' sh {} \; | fzf)
+  session_name=$(basename "$session" | tr ".,: " "____")
 
-  session_name=$(echo "$session" | tr . _)
+  switch_to() {
+    if [[ -z "$TMUX" ]]; then
+      tmux attach-session -t "$session_name"
+    else
+      tmux switch-client -t "$session_name"
+    fi
+  }
 
-  echo "$session_name"
-
-  if ! ${pkgs.tmux}/bin/tmux has-session -t "$session_name" 2> /dev/null; then
-    ${pkgs.tmux}/bin/tmux new-session -s "$session_name" -c "$session" -n '' -d
+  if tmux has-session -t="$session_name" 2>/dev/null; then
+    switch_to
+  else
+    tmux new-session -ds "$session_name" -c "$session"
+    switch_to
   fi
-
-  ${pkgs.tmux}/bin/tmux switch-client -t "$session_name"
-
 ''
