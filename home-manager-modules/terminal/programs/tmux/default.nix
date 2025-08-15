ea934936b00ce0504ee47dbe531acaafa19ed72c
Kyle Ross <github@kylejamesross.com>
2025-08-15 10:43:04 -0600

    Remove tmux bar and add snippet

--- home-manager-modules/terminal/programs/tmux/default.nix
+++ home-manager-modules/terminal/programs/tmux/default.nix
@@ -52,25 +52,7 @@ with config.lib.stylix.colors; {
           run-shell "tmux set-environment -g ANTHROPIC_API_KEY \"\$(cat $HOME/.anthropic_api_key)\""
           run-shell "tmux set-environment -g CLAUDE_API_KEY \"\$(cat $HOME/.anthropic_api_key)\""
 
-          set-option -g status-style "bg=#${base00}"
-          set-window-option -g window-status-format "#[bg=#${base00},fg=#${base0F}]#[bg=#${base0F},fg=#${base00}] #W  #I #[bg=#${base00},fg=#${base0F}]#[bg=#${base00}] "
-          set-window-option -g window-status-current-format "#[bg=#${base00},fg=#${base0B}]#[bg=#${base0B},fg=#${base00}] #W  #I #[bg=#${base00},fg=#${base0B}]#[bg=#${base00}] "
-
-          set -g status-left "#[bg=#${base00}]#[fg=#${base0D}]#{?client_prefix,#[fg=#${base0B}],}#[bg=#${base0D},fg=#${base00}]#{?client_prefix,#[bg=#${base0B}],}   #S   #[bg=#${base00}]#[fg=#${base0D}]#{?client_prefix,#[fg=#${base0B}],}#[bg=#${base00}]  "
-
-          set-option -g pane-border-style "fg=#${base01}"
-          set-option -g pane-active-border-style "fg=#${base03}"
-          set-option -g message-style "fg=#${base05},bg=#${base01}"
-
-          set-option -g display-panes-active-colour "#${base0B}"
-          set-option -g display-panes-colour "#${base0A}"
-
-          set-window-option -g mode-style "fg=#${base04},bg=#${base02}"
-          set-window-option -g window-status-bell-style "fg=#${base01},bg=#${base08}"
-
-          set -g status-position top
-          set -g status-left-length 100
-          set -g status-right ""
+          set -g status off
         '';
       plugins = with pkgs; [
         tmuxPlugins.open
