2c9890a4869ef1614ee990012be1617db291ed99
Kyle Ross <github@kylejamesross.com>
2026-09-14 09:48:09 -0600

    Fix prune branch command

--- home-manager-modules/terminal/scripts/pb/default.nix
+++ home-manager-modules/terminal/scripts/pb/default.nix
@@ -1,4 +1,4 @@
 {pkgs}:
 pkgs.writeShellScriptBin "pb" ''
-  ${pkgs.git}/bin/git branch | ${pkgs.fzf}/bin/fzf --multi | xargs -I {} ${pkgs.git}/bin/git branch -D {}
+  ${pkgs.git}/bin/git branch --format='%(refname:short)' | ${pkgs.fzf}/bin/fzf --multi | xargs -I {} ${pkgs.git}/bin/git branch -D {}
 ''
